5394ff9fcb85ae9d721f9cf110490029a802c615
lordbendtner <nosseir.michel@gmail.com>
2019-10-20 00:04:46 +0200

    update

---
+++ function_ogame.py
@@ -0,0 +1,29 @@
+from ogame import OGame
+from ogame.constants import Ships, Speed, Missions, Buildings, Research, Defense , Facilities
+import time , math
+
+def launch(ogame,id):
+    global_res = ogame.get_resources(id)
+    res_build = ogame.get_resources_buildings(id)
+    lvl_rerearchs = ogame.get_research()
+    lvl_facilities = ogame.get_facilities(id)
+    print(global_res)
+    print(ogame.get_resources_buildings(id))
+    print(lvl_facilities)
+    ogame.build(id, Research['Astrophysics'])
+    ogame.build(id,Facilities['RoboticsFactory'])
+
+    if(global_res['energy'] < 0):
+        ogame.build(id, Buildings['SolarPlant'])
+    elif int(res_build['metal_mine']) < int(res_build['crystal_mine']) + 4:
+        ogame.build(id,Buildings['MetalStorage'])
+        ogame.build(id, Buildings['MetalMine'])
+        print("buildings metal mine...")
+    elif int(res_build['crystal_mine']) < int(res_build['deuterium_synthesizer']) + 4:
+        ogame.build(id,Buildings['CrystalStorage'])
+        ogame.build(id, Buildings['CrystalMine'])
+    else:
+        ogame.build(id, Buildings['DeuteriumTank'])
+        ogame.build(id, Buildings['DeuteriumSynthesizer'])
+
+    time.sleep(1)
--- ogamebot.py
+++ ogamebot.py
@@ -1,61 +1,24 @@
 from ogame import OGame
 from ogame.constants import Ships, Speed, Missions, Buildings, Research, Defense , Facilities
 import time , math
+import function_ogame
 
+print("welcome to ogamebot!!")
 
-def isPayable(i_metal, i_cristal,i_deut,i_lvl,i_coeff, array_res):
-	prixmetal = i_metal*math.pow(i_coeff,i_lvl)
-	prixcrystal = i_cristal*math.pow(i_coeff,i_lvl)
-	prixdeut = i_deut*math.pow(i_coeff,i_lvl)
-	return (prixmetal <= int(array_res['metal']) and prixcrystal <= int(array_res['crystal']) and prixdeut <= int(array_res['deuterium']))
+def global_launch():
+    try:
+        ogame = OGame('Aquarius', "nemesism@hotmail.fr", "pencilcho44")
+        id_pl = ogame.get_planet_ids()
+        i = 0
+        while 1:
+            id = id_pl[i]
+            print(id)
+            function_ogame.launch(ogame,id)
+            i = i + 1
+            time.sleep(1)
+            if i >= len(id_pl):
+                i = 0
+    except:
+        global_launch()
 
-print("welcome")
-ogame = OGame('Aquarius', "nemesism@hotmail.fr", "pencilcho44")
-
-id: int
-id = 34374636
-while 1:
-    global_res = ogame.get_resources(34374636)
-    res_build = ogame.get_resources_buildings(34374636)
-    lvl_rerearchs = ogame.get_research()
-    lvl_facilities = ogame.get_facilities(id)
-    #galaxy_infos = ogame.galaxy_infos(1,30)
-    print(global_res)
-   # print(ogame.get_resources_buildings(34374636))
-    #print(lvl_facilities)
-    print(lvl_rerearchs)
-    #print(galaxy_infos)
-
-    ogame.build(id, Research['Astrophysics'])
-    ogame.build(id,Facilities['RoboticsFactory'])
-    if(global_res['energy'] < 0):
-        if isPayable(75, 15, 0, int(res_build['solar_plant']), 1.5,global_res) == True:
-            ogame.build(id, Buildings['SolarPlant'])
-
-    elif int(res_build['metal_mine']) < int(res_build['crystal_mine']) + 4:
-        ogame.build(id, Buildings['MetalMine'])
-        ogame.build(id,Buildings['MetalStorage'])
-        print("buildings metal mine...")
-    elif int(res_build['crystal_mine']) < int(res_build['deuterium_synthesizer']) + 4:
-        ogame.build(id, Buildings['CrystalMine'])
-        ogame.build(id,Buildings['CrystalStorage'])
-    else:
-        ogame.build(id, Buildings['DeuteriumSynthesizer'])
-        ogame.build(id, Buildings['DeuteriumTank'])
-
-    if(int(lvl_facilities['research_lab']) < 4):
-        ogame.build(id,Facilities['ResearchLab'])
-
-
-    ogame.build(id,Research['ArmourTechnology'])
-
-    if(int(lvl_rerearchs['energy_technology']) < 2):
-        ogame.build(id,Research['EnergyTechnology'])
-    elif((int(lvl_rerearchs['impulse_drive'])) < 3):
-        ogame.build(id, Research['ImpulseDrive'])
-    elif(int(lvl_rerearchs['espionage_technology']) < 5):
-        ogame.build(id,Research['EspionageTechnology'])    
-
-    time.sleep(1)
-
- #1:31
+global_launch()
