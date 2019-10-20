bc07643ec719307e41d917a18c11ffe6bb7df150
lordbendtner <nosseir.michel@gmail.com>
2019-10-20 21:06:46 +0200

    up

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask import render_template
-import ogamebot
+from ogamebot import Afficheur
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return render_template('index.html',ogame=ogamebot.ogame_infos)
+    thread_1 = Afficheur("1")
+    thread_1.start()
+    return render_template('index.html',ogame=Afficheur.ogame_infos)
 
 if __name__ == '__main__':
     app.run()
--- ogamebot.py
+++ ogamebot.py
@@ -2,27 +2,43 @@ from ogame import OGame
 from ogame.constants import Ships, Speed, Missions, Buildings, Research, Defense , Facilities
 import time , math
 import function_ogame
+import random
+import sys
+from threading import Thread
 
 
 print("welcome to ogamebot!!")
-ogame_infos = []
+class Afficheur(Thread):
+    
+    """Thread chargé simplement d'afficher une lettre dans la console."""
 
-def global_launch():
-    #try:
-        ogame = OGame('Aquarius', "nemesism@hotmail.fr", "pencilcho44")
-        id_pl = ogame.get_planet_ids()
-        print(id_pl)
-        i = 0
-        while 1:
-            id = id_pl[i]
-            print(id)
-            ogame_infos = function_ogame.launch(ogame,id)
-            print(ogame_infos)
-            i = i + 1
-            time.sleep(1)
-            if i >= len(id_pl):
-                i = 0
-    #except:
-    #    global_launch()
+    ogame_infos = []
 
-global_launch()
+    def __init__(self, lettre):
+        Thread.__init__(self)
+        self.lettre = lettre
+
+    def run(self):
+        """Code à exécuter pendant l'exécution du thread."""
+        self.global_launch()
+
+    def global_launch(self):
+        #try:
+            ogame = OGame('Aquarius', "nemesism@hotmail.fr", "pencilcho44")
+            id_pl = ogame.get_planet_ids()
+            print(id_pl)
+            i = 0
+            while 1:
+                id = id_pl[i]
+                print(id)
+                ogame_infos = function_ogame.launch(ogame,id)
+                print(ogame_infos)
+                i = i + 1
+                time.sleep(1)
+                if i >= len(id_pl):
+                    i = 0
+        #except:
+        #    global_launch()
+
+#thread_1.join()
+#global_launch()
