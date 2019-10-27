a5381e13b1ea7388e5170029f0893c513a4b4622
lordbentner <nosseir.michel@gmail.com>
2019-10-28 00:46:14 +0100

    pour open

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def stop():
 
 if __name__ == '__main__':
     launch_ogame()
-    app.run(use_reloader = True)
+    app.run(use_reloader = True,host='0.0.0.0', port=PORT)
 
 #use_reloader = True,
 #host='0.0.0.0', port=PORT
